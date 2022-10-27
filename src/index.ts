325f43a6aaf18cdfb00624d995aebae2ab582717
nairol <f.bock.2003@gmail.com>
2022-10-27 22:37:44 +0200

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import 'dotenv/config';
-import autoCheck from './functions/autoCheck';
 import { connectToMongo } from './functions/connectToMongo';
+import TrackPrice from './functions/TrackPrice';
 import productModel from './models/productModel';
+
+const hourInMs = 3.6e6;
 
 async function main() {
 	await connectToMongo();
+	const products = await productModel.find({});
+	console.log(`${new Date().toLocaleTimeString('de-DE', { timeZone: 'Europe/Berlin' })} > Setting up Price Check with Interval of 1 Hour for ${products.length} Items...`);
 
-	const items = await productModel.find({});
+	setTimeout(() => {
+		for (const product of products) {
+			new TrackPrice(product);
+		}
+	}, 5000);
 
-	for (const item of items) {
-		autoCheck(item);
-	}
+	setInterval(() => {
+		for (const product of products) {
+			new TrackPrice(product);
+		}
+	}, hourInMs);
 }
 
 main();
